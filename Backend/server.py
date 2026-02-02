a5fdf7d81fca9fb32938f7919a48b781e6281e27
Abhishek Rawat <rawatabhishek.123321@gmail.com>
2026-02-02 19:44:30 +0530

    postman

--- Backend/server.py
+++ Backend/server.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from datetime import date
 import db_helper
+from pydantic import BaseModel
+
+class Expense(BaseModel):
+    amount: float
+    category: str
+    notes: str
 app = FastAPI()
 
-@app.get("/expenses/{expense_date}")
+@app.get("/expenses/{expense_date}",response_model=list[Expense])
 def get_expense(expense_date: date):
     expenses =  db_helper.fetch_expenses(expense_date)
 
     return expenses
 
+@app.post("/expenses/{expense_date}")
+def add_or_update_expense(expense_date: date,expenses:list[Expense]):
+    for expense in expenses :
+        db_helper.insert_expense(expense_date,expense.amount,expense.notes)
