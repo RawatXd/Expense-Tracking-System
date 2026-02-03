595856c06e8c7681ab321c61270b6d91a211fbb4
Abhishek Rawat <rawatabhishek.123321@gmail.com>
2026-02-04 01:31:29 +0530

    Frontend and Backend

--- Backend/server.py
+++ Backend/server.py
@@ -1,18 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from datetime import date
 import db_helper
 from pydantic import BaseModel
+
+app = FastAPI()
 
 class Expense(BaseModel):
     amount: float
     category: str
     notes: str
-app = FastAPI()
+
+class DateRange(BaseModel):
+    start_date: date
+    end_date : date
+
 
 @app.get("/expenses/{expense_date}",response_model=list[Expense])
 def get_expense(expense_date: date):
     expenses =  db_helper.fetch_expenses(expense_date)
-
+    if expenses is None:
+        raise HTTPException(status_code=500, detail="Failed to retrieve expenses database")
     return expenses
 
 @app.post("/expenses/{expense_date}")
@@ -23,5 +30,21 @@ def add_or_update_expense(expense_date: date,expenses:list[Expense]):
 
     return {"message":"Expense updated successfully"}
 
+@app.post("/analytics/")
+def get_analytics(date_range :DateRange):
+    data = db_helper.fetch_expense_summary(date_range.start_date,date_range.end_date)
 
+    if data is None:
+        raise HTTPException(status_code=500, detail="Data not available")
 
+    total =  sum([row['total'] for row in data])
+
+    breakdown = {}
+    for row in data:
+        percentage = (row['total']/total)*100 if total != 0 else 0
+        breakdown[row['category']] = {
+            "total": row['total'],
+            "percentage": percentage,
+        }
+
+    return breakdown
